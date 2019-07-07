e11875696a9800fc8610e7e74b4d6be75cc9cbfb
Anderson Armijos <52385826+AndersonArmijos1996@users.noreply.github.com>
2019-07-07 16:53:13 -0500

    Add files via upload

---
+++ trabajo/AplicativoAu.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AplicativoAu{ 
+    public static viod main (String []args){
+	String marca;
+    String modelo;
+    String color;
+    float velocidadMaxima;
+	Scanner lee=new Scanner (System.in);
+	System.out.println("ingrese la marca");
+	marca=lee.nextString();	
+	System.out.println("ingrese el modelo");
+	modelo=lee.nextString();
+	System.out.println("ingrese el color");
+	color=lee.nextString();
+	System.out.println("ingrese la velocidadMaxima");
+	velocidadMaxima=lee.nextFloat();
+	}
+    
+	}  
